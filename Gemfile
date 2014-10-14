7dc4219fca4522402621e61afd2b976cd2f05634
wordswords <contact@davidcraddock.net>
2014-10-14 18:32:03 +0100

    Part way to changing the way the hive registers, to pass the hostname instead of config values like name and description

--- Gemfile
+++ Gemfile
@@ -12,3 +12,6 @@ gemspec
 
 # To use debugger
 # gem 'debugger'
+
+gem "pry", :group => :development
+
--- lib/devicedb_comms/hive.rb
+++ lib/devicedb_comms/hive.rb
@@ -4,7 +4,7 @@ module DeviceDBComms
   class Hive < DeviceDBComms::Shared
 
     def register(hive_name, hive_description, mac_address, ip_address)
-      post("/hives/register", { hive: { name: hive_name, description: hive_description, mac: mac_address, ip_address: ip_address }})
+      post("/hives/register", { hive: { hostname: hive_name, mac: mac_address, ip_address: ip_address }})
     end
 
     def poll(hive_id)
