25fd6c9cb7c774abb99c4132c0a800b57fb07140
Joe Haig <joe.haig@bbc.co.uk>
2015-02-17 11:29:31 +0000

    Add method for returning hive details

--- lib/devicedb_comms/hive.rb
+++ lib/devicedb_comms/hive.rb
@@ -3,6 +3,10 @@ require 'devicedb_comms/shared'
 module DeviceDBComms
   class Hive < DeviceDBComms::Shared
 
+    def find(hive_id)
+      get("/hives/#{hive_id}")
+    end
+
     def register(hive_name, mac_address, ip_address)
       post("/hives/register", { hive: { hostname: hive_name, mac: mac_address, ip_address: ip_address }})
     end
