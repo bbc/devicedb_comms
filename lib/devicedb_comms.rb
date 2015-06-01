1bb60ab9cb38690e41b61edda50a1fe3200f8b91
Joe Haig <joe.haig@bbc.co.uk>
2015-06-01 11:13:12 +0100

    More backward compatibility

--- lib/devicedb_comms.rb
+++ lib/devicedb_comms.rb
@@ -7,11 +7,18 @@ require 'devicedb_comms/configuration'
 
 module DeviceDBComms
   class << self
-    attr_accessor :configuration
+    # TODO Add this accessor when the configuration method is removed
+    #attr_accessor :configuration
 
     def configure
       self.configuration = Configuration.new
       yield(configuration)
+    end
+
+    # For backward compatibility
+    # TODO Remove this as the configure method must be called
+    def configuration
+      @configuration ||= Configuration.new
     end
   end
 end
